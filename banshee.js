a7c995225f7cbd7d0a51a49da82ef454b7b225ea
Joe Shaw <joe@joeshaw.org>
2007-06-26 18:55:32 -0400

    Clean up the JS quite a bit, add links for the individual tracks.

--- banshee.js
+++ banshee.js
@@ -1,91 +1,94 @@
-		load_music = function() {
-			$.get("data.html", function(data) {
-				$(".musiclist").append(data);
-				$(".musiclist > tbody > tr > td").mouseover(function() {
-					// change color
-				});
-				$(".musiclist > tbody > tr > td").click(function() {
-					// ajax download additional song info
-					alert("You clicked " + $("this"));
-				});
-			})
-		}
-
-		populate_select = function(select, array) {
-			select.empty();
-			select.unbind("change");
-			
-			for (var i = 0; i < array.length; i++) {
-				var entry = '<option value="' + array[i] + '">' + array[i] + '</option>';
-				select.append(entry);
-			}
-			
-			// For some reason, adding items interactively causes them to
-			// display as though they were selected.  Reset them with this.
-			select[0].selectedIndex = -1;
-		}
-
-		load_artists = function() {
-			$.getJSON("artists.json", function(json) {
-				var artist_list = $("#artist_list");
-
-				populate_select(artist_list, json);
-				
-				artist_list.change(function() {
-					load_albums();
-				});
-
-				// Show up to 20 items
-				artist_list.attr("size", i >= 20 ? 20 : i);
-			})
-		}
-		
-		load_albums = function() {
-			artists = get_selected($("#artist_list"));
-			
-			$.getJSON("albums.json", {"artists" : artists}, function(json) {
-				populate_select($("#album_list"), json);
-				
-				$("#album_list").change(function() {
-					load_tracks();
-				})				
-			});
-		}
-		
-		load_tracks = function() {
-			albums = get_selected($("#album_list"));
-			
-			$.getJSON("tracks.json", {"albums" : albums}, function(json) {
-				populate_table($("#track_table"), json);
-			});
-		}
-				
-		populate_table = function(table, array) {
-			tbody = $("tbody", table);
-			tbody.empty();
-
-			for (var i = 0; i < array.length; i++) {
-				var entry = '<tr id="' + array[i]["id"] + '"><td>' + array[i]["number"] + '</td><td>' + array[i]["name"] + '</td><td>' + array[i]["length"] + '</td><td>' + array[i]["artist"] + '</td><td>'+ array[i]["album"] + '</td></tr>';
-
-				tbody.append(entry);
-			}
-		}
-		
-		get_selected = function(select) {
-			list = [];
-			select.children("option[@selected]").each(function() {
-				list.push($(this).val());
-			});
-			return list;
-		}
-		
-		print_selected = function() {
-			var artists = get_selected($("#artist_list"));
-			var albums = get_selected($("#album_list"));
-			alert("Artists: " + artists + "\n" + "Albums: " + albums);
-		}
+function populateSelect (select, array) {
+	select.empty();
+	select.unbind("change");
 	
-		$(document).ready(function() {
-			load_artists();
-		})
+	for (var i = 0; i < array.length; i++) {
+		var entry = '<option value="' + array[i] + '">' + array[i] + '</option>';
+		select.append(entry);
+	}
+	
+	// For some reason, adding items interactively causes them to
+	// display as though they were selected.  Reset them with this.
+	select[0].selectedIndex = -1;
+	
+	return i;
+}
 
+function populateTable (table, array) {
+	tbody = $("tbody", table);
+	tbody.empty();
+
+	function generateTableItem (array, i, field) {
+		return '<td><a href="' + array[i]["href"] + '" target="_blank">' 
+			+ array[i][field] + '</a></td>';
+	}
+
+	for (var i = 0; i < array.length; i++) {
+		var entry = '<tr id="' + array[i]["id"] + '">'
+			+ generateTableItem (array, i, "number")
+			+ generateTableItem (array, i, "name")
+			+ generateTableItem (array, i, "length")
+			+ generateTableItem (array, i, "artist")
+			+ generateTableItem (array, i, "album")
+			+ "</tr>";
+
+		tbody.append(entry);
+	}
+}
+
+function loadArtists () {
+	$("#artist_list").hide();
+	
+	$.getJSON("artists.json", function(json) {
+		var artist_list = $("#artist_list");
+		
+		count = populateSelect(artist_list, json);
+		
+		artist_list.change(function() {
+			loadAlbums();
+		});
+
+		// Show up to 20 items
+		artist_list.attr("size", count >= 20 ? 20 : count);
+		
+		artist_list.show();
+	});
+}
+
+function loadAlbums () {
+	artists = getSelected($("#artist_list"));
+	
+	$.getJSON("albums.json", {"artists" : artists}, function(json) {
+		populateSelect($("#album_list"), json);
+		
+		$("#album_list").change(function() {
+			loadTracks();
+		})				
+	});
+}
+
+function loadTracks () {
+	albums = getSelected($("#album_list"));
+	
+	$.getJSON("tracks.json", {"albums" : albums}, function(json) {
+		populateTable($("#track_table"), json);
+	});
+}
+
+function getSelected (select) {
+	list = [];
+	select.children("option[@selected]").each(function() {
+		list.push($(this).val());
+	});
+	return list;
+}
+
+function printSelected () {
+	var artists = getSelected($("#artist_list"));
+	var albums = getSelected($("#album_list"));
+	alert("Artists: " + artists + "\n" + "Albums: " + albums);
+}
+
+$(document).ready(function() {
+	loadArtists();
+})
